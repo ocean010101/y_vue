67b9b0e8bf499a05f9dc267abebd620996943b68
yangyang.xu <yangyang.xu@wlbb.dnish.org>
2019-12-07 11:49:07 +0800

    Dependency collection
    Signed-off-by: yangyang.xu <yangyang.xu@wlbb.dnish.org>

--- yvue.js
+++ yvue.js
@@ -6,6 +6,10 @@ class YVue {
     this.$data = options.data;
 
     this.observer(this.$data);
+
+    //测试代码
+    new Watcher(this, 'test'); //把当前实例的test属性添加到Watcher
+    this.test; //激活getter函数
   }
   //使传递进来的数据响应化
   observer(value) {
@@ -33,17 +37,53 @@ class YVue {
   defineReactive(obj, key, val) {
     //2. 递归给所有属性添加getter/setter
     this.observer(val);
+
+    //创建Dep实例：Dep实例与key一对一对应
+    const dep = new Dep();
     Object.defineProperty(obj, key, {
       get: function () {
+        //将Dep.target 指向Watcher 实例加入到Dep中
+        Dep.target && dep.addDep(Dep.target);
         return val;
       },
       set: function (newVal) {
         if (newVal != val) {
           val = newVal;
-          console.log(`${key} 属性更新了`);
+          dep.notify();
+          //console.log(`${key} 属性更新了`);
         }
       }
     })
   }
 
+}
+
+//Dep: 管理若干watcher， 它和key是一对一的关系//通知Watcher做更新
+class Dep {
+  constructor() {
+    this.deps = [];
+  }
+  addDep(watcher) {
+    this.deps.push(watcher);
+  }
+  notify() {
+    this.deps.forEach(dep => dep.update())
+  }
+
+
+}
+
+//保存UI中的依赖， 实现update 函数可以更新之
+class Watcher {
+  constructor(vm, key) {
+    this.vm = vm;
+    this.key = key;
+
+    //将当前实例指向Dep.target
+    Dep.target = this;
+
+  }
+  update() {
+    console.log(`${this.key} 属性更新了`);
+  }
 }
